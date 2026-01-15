be65b97a45bbda204c9957cf04f1d98ca565bf6e
Naga Sai Ram <ramhere939@gmail.com>
2026-01-16 00:13:11 +0530

    feat:Added show entries and button

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
-
+let entries = []
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+
+
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
-    count = 0
+   entries.push(count);
+    count = 0;
+    countEl.textContent = count;
 }
+
+function showEntries() {
+    if (entries.length === 0) {
+        saveEl.textContent = "Previous entries: None" ;
+        return ; 
+    }
+
+    let result = "Previous entries: " ;
+    for (let i = 0; i < entries.length; i++) {
+        result += entries[i] + " - " ;
+    }
+    saveEl.textContent = result ;
+}
+
