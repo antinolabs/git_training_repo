803e736b20745e849e78b951dc830f5bdfe71fc3
Varun Saxena <varun.saxena@antino.com>
2026-01-15 16:31:03 +0530

    Added sum of entries functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let sumEl=document.getElementById("sum-el")
+let totalSum=0
+
 let count = 0
+
 
 function increment() {
     count += 1
@@ -11,5 +15,11 @@ function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
+    totalSum += count
+    sumEl.textContent = "Total sum: " + totalSum
     count = 0
+   
 }
+
+
+ 
