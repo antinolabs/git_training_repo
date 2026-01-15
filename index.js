b494f4fe4d6f8aa8f36156e3519d20a994d25bbe
Amrendera Tomar <amrendera.tomar@antino.com>
2026-01-15 17:09:32 +0530

    feat : implement the logic of localstorage , counter name saving list and reset btn logic

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let inputEl = document.getElementById("input-el")
 let count = 0
+
+let storageCount = JSON.parse(localStorage.getItem("count")) || []
+renderStorageCount()
 
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+
+
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
+   let inputName = inputEl.value || "Anonymous"
+   
+   const entry = { name: inputName,  count: count }
+   storageCount.push(entry)
+    localStorage.setItem("count", JSON.stringify(storageCount))
+
+    renderStorageCount()
+    resetCounter()
+}
+
+function renderStorageCount() {
+    saveEl.innerHTML = "" 
+    storageCount.forEach((entry) => {
+        let li = document.createElement("li")
+        li.textContent = `${entry.name}: ${entry.count} rows`
+        saveEl.appendChild(li)
+    })
+}
+
+function resetCounter() {
     count = 0
+    countEl.textContent = 0
+    inputEl.value = ""
 }
