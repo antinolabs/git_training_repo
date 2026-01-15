bec5feb06d73dba649442f72cf579d6a4320a9f9
komal <komal>
2026-01-15 22:01:36 +0530

    Add alert button with click handler

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+function decrement() {
+    count -= 1
+    countEl.textContent = count
+}
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
 function save() {
-    let countStr = count + " - "
+    let countStr = count + " , "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
