a5f94c24bb22931d6b76a2796cdc534728f1b67c
aayush-Antino <aayush.chauhan@antino.com>
2026-01-15 21:46:31 +0530

    feat : add min and max values display to UI

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let savedCounts = []
+let maxValue = 0
+let minValue = 0
 
 function increment() {
     count += 1
@@ -8,8 +11,23 @@ function increment() {
 }
 
 function save() {
+    savedCounts.push(count) // saving the counts into array
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+}
+
+let maxEl = document.getElementById("max-ele")
+let minEl = document.getElementById("min-ele")
+
+// finding max val
+function findMax(){
+    maxValue = Math.max(...savedCounts)
+    maxEl.textContent = maxValue
+}
+// finding the min val
+function findMin(){
+    minValue = Math.min(...savedCounts)
+    minEl.textContent = minValue
 }
