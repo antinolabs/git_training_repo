849a0e59fab3b778f06fd3fe6a8c03b726f8fca8
Darshil Kumar <darshil.kumar@antino.com>
2026-01-15 20:43:20 +0530

    feat/added counter goal

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let goalInput = document.getElementById("goal-input")
 
 function increment() {
     count += 1
     countEl.textContent = count
+
+    let goal = Number(goalInput.value)
+    if (goal && count === goal) {
+        alert("Goal reached: " + goal)
+    }
 }
 
 function save() {
