b4972a2baf9545276bf7b46d8ab7305a192e0244
Pranav Singh <pranav.singh@antino.com>
2026-01-19 10:25:25 +0530

    Added Decrement btn and validation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let errorEl = document.getElementById("error")
 let count = 0
 
 function increment() {
@@ -7,6 +9,13 @@ function increment() {
     countEl.textContent = count
 }
 
+function decrement() {
+    count = count - 1
+    countEl.textContent = count
+    if (count < -10) {
+        errorEl.textContent = 'Decrement can\'t be more than 10!';
+    }
+}
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
