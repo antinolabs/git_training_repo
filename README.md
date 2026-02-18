2d28b969d7bba33acc24ec4294e199a1f69d8de2
paras-antino <paras.mittal@antino.com>
2026-02-18 12:25:19 +0530

    feat: decrement & validation

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## About Repo
 
-This repo is created for Git training purpose in Antino Labs.
-
-Happy Coding!
+This repo contains code for a counter that allows user to count, by incrementing, decrementing and then saving for viewing later, it has all the necessary validations like not allowing user to save or decrement when value is 0 or -ve 
--- index.js
+++ index.js
@@ -7,9 +7,25 @@ function increment() {
     countEl.textContent = count
 }
 
+function decrement(){
+    if (count>0){
+        count -=1
+    countEl.textContent = count
+    }
+    else{
+        alert("value cannot be -ve")
+    }
+    
+}
+
 function save() {
+    if (count==0){
+        alert("nothing can be saved,count is zero")
+        return
+    }
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+    
 }
